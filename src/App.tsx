b5667709929cdc10a082564eddec60c7a27175a2
Jung Hoan Kim <kjunghoan@gmail.com>
2024-07-17 20:28:52 -0400

    add images, make todo lists finish basic framework from navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
 import NavBar from "./components/NavBar";
+import { Route, Routes } from "react-router-dom"
+import AboutMe from "./pages/AboutMe"
+
+//TODO:
+// GLOBAL TODOS:
+// [] somehow make the pages look retro / anolog
+//  [] cardstock-y componants think vintage fujifilm pictures with minimal colour correct
+//  [] add personality
+// ------------------------------------------
+// [] add vim motions to the pages 
+// [] parallax the images as the user scroll down 
+// [] Finish making the pages
+//  [] homepage
+//  [] projects page
+//  [] hobbies page
+//  [] any other page I think to make
 
 export default function App() {
 
   return (
     <div>
       <NavBar/>
-      <h1 className="text-3xl font-bold underlined">
-        Hello World!
-      </h1>
+      <div className="sm:pl-64">
+        <Routes>
+          <Route path="/" element={<AboutMe/>}/>
+          <Route path="/aboutMe" element={<AboutMe/>}/>
+        </Routes>
+      </div>
+
     </div>
   )
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,58 @@
-export default function NavBar () {
-  return <nav>
-    <h1>This is a navbar</h1>
-  </nav>
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+// TODO:
+// finish css
+// - maybe make item text larger
+// - change hover effect
+// - maybe change font ???
+// upload a picture for side nav
+// add socials (probably import logos/sgvs)
+
+export default function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const navBarItems = [ 
+    {
+      title: "HOME",
+      route: "/",
+    },
+    {
+      title: "ABOUT ME",
+      route: "/aboutMe",
+    },
+    // {
+    //   title: "Projects",
+    //   route: "/projects",
+    // },
+    // {
+    //   title: "Contact Me",
+    //   route: "/contact",
+    // },
+  ];
+
+  return (
+    <div className="fixed w-64 bg-gray-800 text-white h-screen left-0 top-0 overflow-y-auto">
+      <div className="p-4">
+        <div id="profilePicture">
+          <img id="make this round, relative size and remove if the screen too small"/>
+          <h2 className="text-2xl font-bold- mb-4">Jung Hoan Kim</h2> 
+        </div>
+        <nav>
+          <ul>
+            {navBarItems.map((nbi)=> {
+              return (
+                <Link to={nbi.route}  className="mb-2 hover:text-gray-300" key={navBarItems.indexOf(nbi)} >
+                  <li>{nbi.title}</li>
+                </Link>
+              )
+            })}
+          </ul>
+        </nav>
+      </div>
+
+      <div className="absolute bottom-4 left-4">
+        <h1>LIST OF SOCIALS</h1>
+      </div>
+    </div>
+  )
 }
