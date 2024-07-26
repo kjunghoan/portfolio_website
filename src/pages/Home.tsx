9ce2f60233d1ac7fe5f69c2f285db6cded0ba104
Jung Hoan Kim <kjunghoan@gmail.com>
2024-07-26 00:21:28 -0400

    Creating Clean Branch

--- src/pages/Home.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-
-  return (
-    <div>
-      <h1>THIS IS A HOME PAGE</h1>
-    </div>
-  )
-}
--- src/pages/Projects.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Projects() {
-
-  return (
-    <div>
-      <h1>THESE ARE MY PROJECTS</h1>
-    </div>
-  )
-}
